b55fff0828ac4f9d0918cbe54a08d348a13cb798
Ramsheed <medappilramsheed@gmail.com>
2025-11-07 00:14:45 +0400

    Fix duplicate method declaration in Notigen class

--- src/Models/NotificationTemplate.php
+++ src/Models/NotificationTemplate.php
@@ -33,4 +33,34 @@ class NotificationTemplate extends Model
             ->with($data)
             ->render();
     }
+
+    /**
+     * Send notification using this template
+     *
+     * @param mixed $notifiable The entity receiving the notification
+     * @param array $data Template variables
+     * @param array|null $channels Specific channels to use (defaults to template channels)
+     * @return void
+     */
+    public function send($notifiable, array $data, ?array $channels = null)
+    {
+        $notification = new TemplateNotification(
+            $this,
+            $data,
+            $channels ?? $this->channels
+        );
+
+        $notifiable->notify($notification);
+    }
+
+    /**
+     * Find template by name
+     *
+     * @param string $name
+     * @return static|null
+     */
+    public static function findByName(string $name)
+    {
+        return static::where('name', $name)->first();
+    }
 }
---
+++ src/Notifications/TemplateNotification.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Notigen\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Notigen\Models\NotificationTemplate;
+
+class TemplateNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * @var NotificationTemplate
+     */
+    protected $template;
+
+    /**
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * @var array
+     */
+    protected $channels;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(NotificationTemplate $template, array $data, array $channels)
+    {
+        $this->template = $template;
+        $this->data = $data;
+        $this->channels = $channels;
+
+        if (config('notigen.queue_notifications', true)) {
+            $this->onQueue(config('notigen.default_queue', 'default'));
+        }
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     */
+    public function via($notifiable): array
+    {
+        return $this->channels;
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable): array
+    {
+        return [
+            'subject' => $this->renderSubject(),
+            'html' => $this->template->renderContent($this->data),
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     */
+    public function toArray($notifiable): array
+    {
+        return [
+            'template_id' => $this->template->id,
+            'content' => $this->template->renderContent($this->data),
+            'data' => $this->data,
+        ];
+    }
+
+    /**
+     * Get the Slack representation of the notification.
+     */
+    public function toSlack($notifiable): array
+    {
+        return [
+            'content' => $this->template->renderContent($this->data),
+        ];
+    }
+
+    /**
+     * Render the notification subject
+     */
+    protected function renderSubject(): string
+    {
+        return $this->template->name;
+    }
+}
