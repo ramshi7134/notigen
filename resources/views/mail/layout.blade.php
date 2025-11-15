9960f87e443b7edfcf897b26e6256f69357c2aa8
Ramsheed <medappilramsheed@gmail.com>
2025-11-15 23:23:21 +0400

    feat(email): add responsive HTML mail layout to restore formatting

--- resources/views/mail/layout.blade.php
+++ resources/views/mail/layout.blade.php
@@ -1,1 +1,153 @@
-{!! $slot !!}
+<!doctype html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>{{ config('app.name') }}</title>
+    <style>
+        /* Base ------------------------------ */
+        body,
+        body *:not(html):not(style):not(br):not(tr):not(code) {
+            font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;
+            box-sizing: border-box;
+        }
+
+        body {
+            background-color: #f2f4f6;
+            color: #51545E;
+            height: 100%;
+            width: 100% !important;
+            margin: 0;
+            -webkit-text-size-adjust: none;
+        }
+
+        a {
+            color: #3869D4;
+        }
+
+        /* Layout ------------------------------ */
+        .email-wrapper {
+            width: 100%;
+            margin: 0;
+            padding: 0;
+            background-color: #f2f4f6;
+        }
+
+        .email-content {
+            width: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        /* Masthead ----------------------- */
+        .email-masthead {
+            padding: 25px 0;
+            text-align: center;
+        }
+
+        .email-masthead_name {
+            font-size: 16px;
+            font-weight: bold;
+            color: #A8AAAF;
+            text-decoration: none;
+            text-shadow: 0 1px 0 white;
+        }
+
+        /* Body ------------------------------ */
+        .email-body {
+            width: 100%;
+            margin: 0;
+            padding: 0;
+            background-color: #FFFFFF;
+        }
+
+        .email-body_inner {
+            width: 570px;
+            margin: 0 auto;
+            padding: 0;
+        }
+
+        .content-cell {
+            padding: 35px;
+        }
+
+        /* Button ------------------------------ */
+        .button {
+            display: inline-block;
+            padding: 10px 18px;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 4px;
+        }
+
+        .button-primary {
+            background-color: #3869D4;
+        }
+
+        /* Footer ------------------------------ */
+        .email-footer {
+            width: 570px;
+            margin: 0 auto;
+            padding: 35px;
+            text-align: center;
+            color: #A8AAAF;
+        }
+
+        @media only screen and (max-width: 600px) {
+
+            .email-body_inner,
+            .email-footer {
+                width: 100% !important;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <table class="email-wrapper" width="100%" cellpadding="0" cellspacing="0">
+        <tr>
+            <td align="center">
+                <table class="email-content" width="100%" cellpadding="0" cellspacing="0">
+                    <!-- Masthead -->
+                    <tr>
+                        <td class="email-masthead">
+                            <a href="{{ config('app.url') }}" class="email-masthead_name">
+                                {{ config('app.name') }}
+                            </a>
+                        </td>
+                    </tr>
+
+                    <!-- Email Body -->
+                    <tr>
+                        <td class="email-body" width="100%">
+                            <table class="email-body_inner" align="center" width="570" cellpadding="0"
+                                cellspacing="0">
+                                <tr>
+                                    <td class="content-cell">
+                                        {!! $slot !!}
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <!-- Footer -->
+                    <tr>
+                        <td>
+                            <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td class="content-cell" align="center">
+                                        <p>&copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.</p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
